539ff8943a8df564fe6c0055264ec970b430a730
Helen Hastings <hhas77@stanford.edu>
2016-04-06 11:59:40 -0700

    Cleaner communication to reader thread using a queue.

--- gdb_writer.py
+++ gdb_writer.py
@@ -1,37 +1,32 @@
 import subprocess
-from multiprocessing import Process
+import multiprocessing
 import sys
 import os
 
-def output_poll_and_write(read_handle, write_handle):
-    print "in func", read_handle, write_handle
-    for line in read_handle.readlines():
-        print line
-        write_handle.write(line)
-        write_handle.flush()
-
-
-def read_gdb_output(read_fd):
-    print "here"
-    content = ""
-    while "The program is not being run" not in content:
+def read_gdb_output(read_fd, output_queue):
+    while True:
         content = os.read(read_fd, 1000)
-        print content
+        output_queue.put(content)
 
 def debug(file_to_debug):
     output_file = open('output_' + file_to_debug, 'w')
     proc = subprocess.Popen(['gdb', file_to_debug], stdout=subprocess.PIPE,
     stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
     fd = proc.stdout.fileno()
-    writer = Process(target=read_gdb_output, args=[fd])
+    output_queue = multiprocessing.Queue()
+    writer = multiprocessing.Process(target=read_gdb_output, args=[fd, output_queue])
     writer.start()
+
     proc.stdin.write('b main\n')
     proc.stdin.write('run\n')
-#    while proc.poll() is None:
-    for i in range(150):
+    latest_output = output_queue.get()
+    while "exited normally" not in latest_output:
+        print latest_output
         proc.stdin.write('frame\n') 
         proc.stdin.write('next\n')
-    writer.join()
+        latest_output = output_queue.get()
+    print latest_output
+    writer.terminate()
 
 
 def main():
