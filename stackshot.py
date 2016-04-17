07f588d96b548a50f89a8804dad2f8922cf918ec
Helen Hastings <hhas77@stanford.edu>
2016-04-17 15:33:28 -0700

    Add functionality for stackshot to ingest regsiters

--- stackshot.py
+++ stackshot.py
@@ -7,9 +7,9 @@ class StackShot:
 
   def __init__(self):
     self.line = None  # String, last line number
-    self.rsp = None
-    self.rbp = None
     self.regs = {
+      'rsp': None,
+      'rbp': None,
       'rax': None,
       'rbx': None,
       'rcx': None,
@@ -26,8 +26,6 @@ class StackShot:
       'r15': None
     }
     self.words = {}
-    self.rbp_ind = 0
-    self.rsp_ind = 0
 
   def stringify(self):
     return self.line
@@ -35,11 +33,20 @@ class StackShot:
   def ingest(self, data, command):
     try:
       {
-        'step': self.ingest_step
+        'step': self.ingest_step,
+        'info registers': self.ingest_all_registers
       }[command](data)
     except KeyError:
       print "Cannot ingest data from gdb command %s." % command 
     
+  def ingest_all_registers(self, data):
+    if "The program has no registers now" in data:
+      return
+    for register_output in data.split("\n")[:16]: # only want first 16
+      register, contents = register_output.split()[:2]
+      self.regs[register] = contents
+
+
   def ingest_step(self, new_data):
     last_line = new_data.split('\n')[-2] # actual last line is (gdb)
     self.line = last_line
