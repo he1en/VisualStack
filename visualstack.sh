22c72ea92e85648e27cb23e67d7f11f5ae80029e
Rachel Bloch Mellon <rbmellon@stanford.edu>
2016-04-26 10:17:26 -0700

    Compiles .c file in gdb_runner for user and new bash script to run pipeline

---
+++ visualstack.sh
@@ -0,0 +1,19 @@
+#!/bin/hash
+
+if [ $# -ne 1 ]; then
+    echo $0: usage: visualstack.sh filename.c
+    exit 1
+fi
+
+clear
+
+sqlite3 VisualStack.db < vscreate.sql;
+python vsbase.py 8080 $1
+
+#if which xdg-open > /dev/null
+#then
+#  xdg-open myth24.stanford.edu:8080/visualstack
+#elif which gnome-open > /dev/null
+#then
+#  gnome-open myth24.stanford.edu:8080/visualstack
+#fi
