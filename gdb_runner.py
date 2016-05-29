a026fcefd21467c6aa4c693b82e15938934d509e
Rachel Bloch Mellon <rbmellon@stanford.edu>
2016-05-29 16:15:54 -0700

    Removed unnecessary print statements/comments.

--- gdb_runner.py
+++ gdb_runner.py
@@ -84,9 +84,7 @@ class GDBRunner:
     self.capture_stack()
 
     vsdb.setStep(self.step_num, self.step_i)
-    #self.step_num = -1
     self.step_num_i = -1
-    # vsdb.runnerStep(self.step_num, self.step_i, self.parser.get_stackshot())
 
   def capture_stack(self):
     for command in self.parser.get_context_commands():
@@ -100,9 +98,6 @@ class GDBRunner:
    
     self.send(self.parser.step_command)
     self.capture_stack()
-    print self.step_num
-    print self.parser.new_line
-
 
     if self.parser.first_time_new_function():
       t = vsdb.transaction()
@@ -119,9 +114,8 @@ class GDBRunner:
       self.step_i = 0
     else:
       self.step_i += 1    
-    vsdb.runnerStep(self.step_num, self.step_i, self.parser.get_stackshot())
-    #self.step_i += 1
 
+    vsdb.runnerStep(self.step_num, self.step_i, self.parser.get_stackshot())
 
   def run_to_completion(self):
     ''' To be called AFTER start. '''
